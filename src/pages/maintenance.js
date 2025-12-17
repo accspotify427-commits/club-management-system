9a93ba59df422e4f26fc6299d717ddc355a657c4
accspotify427-commits <accspotify427@gmail.com>
2025-12-17 20:13:35 +0530

    Add maintenance page

---
+++ src/pages/maintenance.js
@@ -0,0 +1,11 @@
+export function renderMaintenance() {
+  return `
+    <div class="maintenance-container">
+      <div class="maintenance-card">
+        <h1>🔧</h1>
+        <h1>Under Maintenance</h1>
+        <p>We're currently performing scheduled maintenance. Please check back soon!</p>
+      </div>
+    </div>
+  `;
+}
